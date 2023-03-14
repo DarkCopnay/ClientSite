44d4d5514af0d0b905600679b295162006d6e76a
Maksim <56311335+remix2021687@users.noreply.github.com>
2023-03-14 16:30:27 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,24 +1,32 @@
-// let sliderphoto = document.getElementById("sliderphoto");
-// let list_position = 0;
+window.addEventListener("load", function() {
+  Slider();
+  ModelWindow();
 
-// let button_left = document.getElementById("button_left");
-// let button_right = document.getElementById("button_right");
+})
 
-// let sliderInterval = setInterval(function() {
-//   sliderphoto.style.transform = `translateX(-${list_position += 600}px)`;
+
+function Slider() {
+  let sliderphoto = document.getElementById("sliderphoto");
+  let list_position = 0;
+
+  let sliderInterval = setInterval(function() {
+    sliderphoto.style.transform = `translateX(-${list_position += 600}px)`;
   
-//   if (list_position == 1200){
-//     list_position -= 2400;
-//   }
-// }, 4500);
+    if (list_position == 1200){
+      list_position -= 2400;
+    }
+  }, 4500);
+}
 
-let button_dropmenu = document.getElementById("button_dropmenu");
-let topnav_dropmenu = document.getElementById("topnav_dropmenu");
+function ModelWindow() {
+  let button_dropmenu = document.getElementById("button_dropmenu");
+  let topnav_dropmenu = document.getElementById("topnav_dropmenu");
 
-button_dropmenu.addEventListener("focusin", function() {
-  topnav_dropmenu.style.height = "150px";
-})
+  button_dropmenu.addEventListener("focusin", function() {
+    topnav_dropmenu.style.height = "150px";
+  });
 
-button_dropmenu.addEventListener("focusout", function() {
-  topnav_dropmenu.style.height = "0";
-})
+  button_dropmenu.addEventListener("focusout", function() {
+    topnav_dropmenu.style.height = "0";
+  });
+}
